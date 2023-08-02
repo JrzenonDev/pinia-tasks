aa0abad3157ccebcfab03c9491634fe1c1127c05
JrzenonDev <jribertoonb@gmail.com>
2023-08-02 12:43:38 -0300

    feta: use component TaskDetails and store TaskStore

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,26 @@
   <main>
     <header>
       <img src="./assets/pinia-logo.svg" alt="Pinia logo" />
-      <h1>Pinia Tasks</h1>
+      <h1>Pinia Tasks {{ taskStore.name }}</h1>
     </header>
+
+    <div class="task-list">
+      <div v-for="task in taskStore.tasks">
+        <TaskDetails :task="task" />
+      </div>
+    </div>
   </main>
 </template>
 
 <script>
-export default {};
+import { useTasksStore } from "./store/TasksStore";
+import TaskDetails from "./components/TaskDetails.vue";
+
+export default {
+  components: { TaskDetails },
+  setup() {
+    const taskStore = useTasksStore();
+    return { taskStore };
+  },
+};
 </script>
