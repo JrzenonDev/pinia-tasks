d743d9fadcdc9dd68e840393a9aa32a4ccf463b1
JrzenonDev <jribertoonb@gmail.com>
2023-08-02 11:55:57 -0300

    feat: initial commit

---
+++ src/router/index.js
@@ -0,0 +1,21 @@
+import HomeView from "../views/HomeView.vue";
+import FavoritesView from "../views/FavoritesView.vue";
+import { createRouter, createWebHistory } from "vue-router";
+
+const routes = [
+  {
+    path: "/",
+    component: HomeView,
+  },
+  {
+    path: "/favorites",
+    component: FavoritesView,
+  },
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
+export default router;
