9ca34ae924b5972e634b982b09b0849445fd4c69
JrzenonDev <jribertoonb@gmail.com>
2023-08-02 23:35:27 -0300

    feat: use actions store deleteTask and toggleFav in TaskDetails

--- src/components/TaskDetails.vue
+++ src/components/TaskDetails.vue
@@ -2,14 +2,23 @@
   <div class="task">
     <h3>{{ task.title }}</h3>
     <div class="icons">
-      <i class="material-icons">delete</i>
-      <i class="material-icons">favorites</i>
+      <i class="material-icons" @click="taskStore.deleteTask(task.id)">
+        delete
+      </i>
+      <i class="material-icons" @click="taskStore.toggleFav(task.id)">
+        favorites
+      </i>
     </div>
   </div>
 </template>
 
 <script>
+import { useTasksStore } from "../store/TasksStore";
 export default {
   props: ["task"],
+  setup() {
+    const taskStore = useTasksStore();
+    return { taskStore };
+  },
 };
 </script>
